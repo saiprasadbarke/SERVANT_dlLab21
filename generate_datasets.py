d0e4662cec275c89e75b2da006739d8024822537
saiprasad_barke <barke.saiprasad1992@gmail.com>
2021-06-14 12:35:08 +0530

    Created skeleton for generate_datasets.py First commit

---
+++ generate_datasets.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class GenerateDatasets:
+
+    def __init__(self, equations: List[List[str]] = None, X_values: List[str] = None):
+        self.equations = equations
+        self.X_values = X_values
+    
+    def generate_xy_dataset(self, equation: List[str] = None) -> int:
+        
+        
+
+
+        return 0
+
+
+
+
+
