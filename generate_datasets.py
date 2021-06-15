8ee5466a5548a16705b1283220c9d3b24df2de63
saiprasad_barke <barke.saiprasad1992@gmail.com>
2021-06-15 12:16:52 +0530

    Fancier wrapper for the same noob methodology

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,39 +1,51 @@
-from typing import List
+from typing import List, Dict
 from numpy.random import uniform
-class GenerateDatasets:
+from json import dumps
 
-    OPERATORS = ["+", "-", "*", "/"]
-    def __init__(self, equations: List[List[str]] = None, X_values: List[str] = None):
+
+class GenerateDatasets:
+    def __init__(self, equations: List[List[str]] = None, X_values: List[float] = None):
         self.equations = equations
         self.X_values = X_values
-    
-    def generate_xy_dataset(self) -> float:
-        
+
+    def generate_xy_datasets(self) -> Dict[str, Dict[float, float]]:
+        """
+        This method calls generate_xy_dataset_for_equation for every equation and returns a dictionary with the key as the equation in string form and the value as the dictionary of X-Y values for the said equation.
+        """
+        dictionary_of_xy_values_for_equations = dict()
+        for equation in self.equations:
+            dictionary_of_xy_values_for_equations[
+                f"{''.join(equation)}"
+            ] = self.generate_xy_dataset_for_equation(equation)
+        return dictionary_of_xy_values_for_equations
+
+    def generate_xy_dataset_for_equation(
+        self, equation: List[str]
+    ) -> Dict[float, float]:
+        """
+        This method returns a dictionary of X-Y values for a given equation
+        """
+        dictionary_of_xy_values = dict()
         for x in self.X_values:
-            for equation in self.equations:
-                expression = [] 
-                for token in equation:
-                    if token in GenerateDatasets.OPERATORS:
-                        expression.append(token)
-                    elif token == "x":
-                        expression.append(f"{x}")
-                    else:
-                        expression.append(token)
-                
-                #print(f"Expression as list is: {expression}")
-                expression = "".join(expression)
-                #print(f"Expression as string is: {expression}")
-                value = eval(expression)
-                print(f"Value of expression {''.join(equation)} for X = {x} is {value}")
-        return 0
+            expression = []
+            for token in equation:
+                if token == "x":
+                    expression.append(f"{x}")
+                else:
+                    expression.append(token)
+            expression = "".join(expression)
+            value = eval(expression)
+            dictionary_of_xy_values[x] = value
+        return dictionary_of_xy_values
+
 
 if __name__ == "__main__":
-    list_of_equations = [["3","*","x","*","x","+","2","*","x","+","1"], ["5","*","x","-","3"]]
+    list_of_equations = [
+        ["3", "*", "x", "*", "x", "+", "2", "*", "x", "+", "1"],
+        ["5", "*", "x", "-", "3"],
+    ]
     X_values = uniform(-1, 1, 5)
-    dataset_generator = GenerateDatasets(equations= list_of_equations, X_values=X_values)
-    dataset_generator.generate_xy_dataset()
-
-
-
-
-
+    dataset_generator = GenerateDatasets(equations=list_of_equations, X_values=X_values)
+    datesets_dict = dataset_generator.generate_xy_datasets()
+    # dumps makes the float X values in keys into strings
+    print(dumps(datesets_dict, indent=4))
